8052a6fb2d3b18d55f6999a3fcfec7118087e18c
subo <kws3363@gmail.com>
2022-01-01 20:48:43 +0900

    @Transactionl

---
+++ src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
@@ -0,0 +1,35 @@
+package com.fastcampus.jpa.bookmanager.service;
+
+import com.fastcampus.jpa.bookmanager.domain.Author;
+import com.fastcampus.jpa.bookmanager.domain.Book;
+import com.fastcampus.jpa.bookmanager.repository.AuthorRepository;
+import com.fastcampus.jpa.bookmanager.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+
+//    @Transactional(rollbackFor = Exception.class)
+    @Transactional
+    public void putBookAndAuthor() throws Exception {
+        Book book = new Book();
+        book.setName("JPA 시작하기");
+
+        bookRepository.save(book);
+
+        Author author = new Author();
+        author.setName("martin");
+
+        authorRepository.save(author);
+
+        throw new Exception("오류가 나서 DB commit이 발생하지 않습니다.");
+    }
+
+}
