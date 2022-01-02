f4a2e3fd98469d8abed591419af8fbd91534dee2
subo <kws3363@gmail.com>
2022-01-02 20:40:31 +0900

    transaction-manager

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/Book.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/Book.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -22,6 +23,7 @@ import java.util.List;
 @Data
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
+//@DynamicUpdate 필요한 정보만 업데이트
 public class Book extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
@@ -7,29 +7,59 @@ import com.fastcampus.jpa.bookmanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
-
+    private final EntityManager entityManager;
+    private final AuthorService authorService;
 
 //    @Transactional(rollbackFor = Exception.class)
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void putBookAndAuthor() throws Exception {
         Book book = new Book();
         book.setName("JPA 시작하기");
 
         bookRepository.save(book);
+        authorService.putAuthor();
+        try{
+            authorService.putAuthor();
+        }catch (RuntimeException e){
 
-        Author author = new Author();
-        author.setName("martin");
+        }
+//
+//        Author author = new Author();
+//        author.setName("martin");
+//
+//        authorRepository.save(author);
+//
+////        throw new Exception("오류가 나서 DB commit이 발생하지 않습니다.");
+//        throw new RuntimeException("오류가 발생하였습니다. transaction은 어덯게 될까요?");
 
-        authorRepository.save(author);
+    }
 
-        throw new Exception("오류가 나서 DB commit이 발생하지 않습니다.");
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void get(Long id) {
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        entityManager.clear();
+
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        bookRepository.update();
+//        Book book = bookRepository.findById(id).get();
+//        book.setName("바뀔까?");
+//        bookRepository.save(book);
+
     }
 
 }
