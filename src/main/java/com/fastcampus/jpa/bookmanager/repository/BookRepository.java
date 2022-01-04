ceee508e4d3be9d34253f131cc23ba2b791260dc
subo <kws3363@gmail.com>
2022-01-04 17:18:55 +0900

    @Query

--- src/main/java/com/fastcampus/jpa/bookmanager/repository/BookRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/repository/BookRepository.java
@@ -2,12 +2,14 @@ package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.Book;
 import com.fastcampus.jpa.bookmanager.repository.dto.BookNameAndCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Tuple;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -34,7 +36,30 @@ public interface BookRepository extends JpaRepository<Book,Long> {
             @Param("createdAt") LocalDateTime createdAt,
             @Param("updatedAt") LocalDateTime updatedAt);
 
-    @Query(value = "select b.name as name, b.category as category from Book b")
-    List<Tuple> findBookNameAndCategory();
+
+    //jpql
+    @Query(value = "select new com.fastcampus.jpa.bookmanager.repository.dto.BookNameAndCategory(b.name , b.category) from Book b")
+    List<BookNameAndCategory> findBookNameAndCategory();
+
+    //paging, message overloading
+    @Query(value = "select new com.fastcampus.jpa.bookmanager.repository.dto.BookNameAndCategory(b.name , b.category) from Book b")
+    Page<BookNameAndCategory> findBookNameAndCategory(Pageable pageable);
+
+    @Query(value = "select * from book", nativeQuery = true)
+    List<Book> findAllCustom();
+
+    @Transactional
+    @Modifying
+    @Query(value = "update book set category = 'IT전문서'", nativeQuery = true)
+    int updateCategories();
+    //void를 해도되지만 int,long으로 선언시엔 dml 인경우 update row 수를 리턴 받는다.(affected rows)
+    //update delete 쿼리는 @Modyfing을 선언해줘야한다.
+    //natviequery실행시에는 @Transactionl을 선언해줘야한다.
+
+    @Query(value = "show tables", nativeQuery = true)
+    List<String> showTables();
+
+
+
 
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
@@ -7,9 +7,11 @@ import com.fastcampus.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class BookRepositoryTest {
@@ -127,10 +129,34 @@ class BookRepositoryTest {
                 LocalDateTime.now().minusDays(1L)));
 
         System.out.println(bookRepository.findBookNameAndCategory());
-        bookRepository.findBookNameAndCategory().forEach(tuple -> {System.out.println(tuple.get(0)+ " : " + tuple.get(1));});
+//        bookRepository.findBookNameAndCategory().forEach(tuple -> {System.out.println(tuple.get(0)+ " : " + tuple.get(1));});
+        bookRepository.findBookNameAndCategory().forEach(b -> {System.out.println(b.getName() + " : " + b.getCategory());});
+
+        bookRepository.findBookNameAndCategory(PageRequest.of(1, 1)).forEach(
+                bookNameAndCategory -> System.out.println(bookNameAndCategory.getName() + " : " + bookNameAndCategory.getCategory()));
+
+        bookRepository.findBookNameAndCategory(PageRequest.of(0, 1)).forEach(
+                bookNameAndCategory -> System.out.println(bookNameAndCategory.getName() + " : " + bookNameAndCategory.getCategory()));
+
     }
 
+    @Test
+    void nativeQueryTest() {
+//        bookRepository.findAll().forEach(System.out::println);
+//        bookRepository.findAllCustom().forEach(System.out::println);
 
+        List<Book> books = bookRepository.findAll();
+
+        for(Book book : books) {
+            book.setCategory("IT전문서적");
+        }
+        bookRepository.saveAll(books);
+        System.out.println(bookRepository.findAll());
+        System.out.println("affected rows : " + bookRepository.updateCategories());
+        bookRepository.findAll().forEach(System.out::println);
+
+        System.out.println(bookRepository.showTables());
+    }
 
     private void givenBookAndReview() {
         givenReview(givenUser(),givenBook(givenPublisher()));
